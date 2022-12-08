4b87c692be7b52a4d96879e3ffeb0e8789adacb0
Patossium <bliuv.simona@gmail.com>
2022-12-09 01:52:33 +0200

    Updated input error catching for clock task.

--- ClockTask/ClockTask/Program.cs
+++ ClockTask/ClockTask/Program.cs
@@ -1,47 +1,72 @@
-﻿namespace ClockTask
+﻿using System.Text;
+namespace ClockTask
 {
     internal class Program
     {
+        private const double ONE_HOUR_DEGREES = 360 / 12;
+        private const double ONE_MINUTE_DEGREES = 360 / 60;
         public static void Main(string[] args)
         {
-            double oneHourDegrees = 360 / 12;
-            double oneMinuteDegrees = 360 / 60;
+            
             double inputHour;
             double inputMinute;
+            bool minutes, hours; //will save information if the console input was successfully parsed
 
             Console.WriteLine("Enter the value of the hour hand: ");
-            inputHour = double.Parse(Console.ReadLine());
+            hours = double.TryParse(Console.ReadLine(), out inputHour);
             Console.WriteLine("Enter the value of the minute hand: ");
-            inputMinute = double.Parse(Console.ReadLine());
-            if (inputHour > 24 && inputMinute > 60)
+            minutes = double.TryParse(Console.ReadLine(), out inputMinute);
+
+            if (hours == false || minutes == false) //checks if the input was a number or not
+            {
+                Console.WriteLine("Please enter a number when defining the values of both clock hands");
+                return;
+            }
+
+            if (inputHour > 24 && inputMinute > 60) //checks if both inputs have a valid value
             {
                 Console.WriteLine("Please enter valid values for both of the variables");
                 return;
             }
-            if (inputHour > 24)
+            
+            if (inputHour > 24) //checks if the hour input has a valid value
             {
                 Console.WriteLine("Please enter the hour hand value that is between 0 and 24.");
                 return;
             }
-            if(inputMinute > 60)
+
+            if(inputMinute > 60) //checks if the minute input has a valid value
             {
                 Console.WriteLine("Please enter the minute hand value that is between 0 and 60");
                 return;
             }
-            double angle = LesserAngleCalculation(oneHourDegrees, oneMinuteDegrees, inputHour, inputMinute);
+
+            if (inputHour < 0 || inputMinute < 0) //checks if the input is negative
+            {
+                Console.WriteLine("Please enter a value that is above 0");
+                return;
+            }
+
+            double angle = LesserAngleCalculation(inputHour, inputMinute);
             Console.WriteLine("The lesser angle between the hour and minute hands is: ");
             Console.WriteLine(angle);
 
         }
-        public static double LesserAngleCalculation(double oneHourDegrees, double oneMinuteDegrees, double inputHour, double inputMinute)
+        /// <summary>
+        /// Calculates the lesser angle between the two hands of the clock
+        /// </summary>
+        /// <param name="inputHour">the value of the hour hand that the user has written</param>
+        /// <param name="inputMinute">the value of minute hand that the user has written</param>
+        /// <returns>the calculated angle between the two hands of the clock</returns>
+        public static double LesserAngleCalculation(double inputHour, double inputMinute)
         {
             double angleResult = 0;
             if(inputHour > 12)
             {
                 inputHour -= 12;
             }
-            double hourAngle = oneHourDegrees * inputHour + (oneHourDegrees / 60 * inputMinute);
-            double minuteAngle = oneMinuteDegrees * inputMinute;
+            double hourAngle = ONE_HOUR_DEGREES * inputHour + (ONE_HOUR_DEGREES / 60 * inputMinute);
+            double minuteAngle = ONE_MINUTE_DEGREES * inputMinute;
             if(hourAngle > minuteAngle)
             {
                 angleResult = hourAngle - minuteAngle;
