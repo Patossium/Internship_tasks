cb18b83a16e4d9ced30f2d3c9cfbf5dbb5b2bff4
Patossium <bliuv.simona@gmail.com>
2022-12-09 02:31:57 +0200

    Branch methods moved outside of the Branch class

--- Data_Tree_Depth_Calculation/Data_Tree_Depth_Calculation/Program.cs
+++ Data_Tree_Depth_Calculation/Data_Tree_Depth_Calculation/Program.cs
@@ -7,59 +7,68 @@ namespace Program
 {
     internal class Code
     {
-        const int maxBranchAmount = 2; //maximum amount of leafs that a branch can have
-        const int branchesToAdd = 50; //amount of branches that will be randomly added to the data tree
+        private const int maxBranchAmount = 2; //maximum amount of leafs that a branch can have
+        private const int branchesToAdd = 50; //amount of branches that will be randomly added to the data tree
         static void Main(string[] args)
         {
             Branch branches = new Branch();
             Random rand = new Random();
-            for(int i = 0; i < branchesToAdd; i++)
+            for(int i = 0; i < branchesToAdd; i++) //Randomly generates a data tree with a preset amount of branches
             {
-                branches.AddBranch();
+                AddBranch(branches);
             }
-            int maxTreeDepth = branches.FindTreeDepth(0);
+            int maxTreeDepth = FindTreeDepth(branches, 0);
             Console.WriteLine("The maximum depth of the current randomly generated data tree is: {0}", maxTreeDepth);
-
-            branches.PrintTreeToConsole();
         }
         class Branch
         {
-            List<Branch> branches = new List<Branch>();
+            public List<Branch> branches { get; set; }
 
-            /// <summary>
-            /// Recursively adds a set amount of branches to the data tree, however the depth is never known
-            /// </summary>
-            public void AddBranch()
+            public Branch(List<Branch> branches)
             {
-                Random rnd = new Random();
-                int randomBranchIndex = rnd.Next(maxBranchAmount);
-                if (branches.Count > randomBranchIndex)
+                this.branches = branches;
+            }
+            public Branch()
+            {
+                this.branches = new List<Branch>();
+            }
+        }
+        /// <summary>
+        /// Adds a new branch to a random branch
+        /// </summary>
+        /// <param name="branch">data tree branch</param>
+        static void AddBranch(Branch branch)
+        {
+            Random rnd = new Random();
+            int randomBranchIndex = rnd.Next(maxBranchAmount);
+            if (branch.branches.Count > randomBranchIndex)
+            {
+                AddBranch(branch.branches[randomBranchIndex]);
+            }
+            else
+            {
+                branch.branches.Add(new Branch());
+            }
+        }
+        /// <summary>
+        /// Finds the maximum depth of the data tree
+        /// </summary>
+        /// <param name="branch">data tree branch</param>
+        /// <param name="currentDepth">the current depth</param>
+        /// <returns>the maximum depth of the data tree</returns>
+        static int FindTreeDepth(Branch branch, int currentDepth)
+        {
+            int depth = currentDepth + 1;
+            int maxDepth = depth;
+            foreach (Branch branch1 in branch.branches)
+            {
+                int branchDepth = FindTreeDepth(branch1, depth);
+                if (branchDepth > maxDepth)
                 {
-                    branches[randomBranchIndex].AddBranch();
-                }
-                else
-                {
-                    branches.Add(new Branch());
+                    maxDepth = branchDepth;
                 }
             }
-            public int FindTreeDepth(int currentDepth)
-            {
-                int depth = currentDepth + 1;
-                int maxDepth = depth;
-                foreach (Branch branch in branches)
-                {
-                    int branchDepth = branch.FindTreeDepth(depth);
-                    if(branchDepth > maxDepth)
-                    {
-                        maxDepth = branchDepth;
-                    }
-                }
-                return maxDepth;
-            }
-            public void PrintTreeToConsole()
-            {
-                
-            }
+            return maxDepth;
         }
     }
 }
